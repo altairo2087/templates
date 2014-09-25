e133b6b6ba3dd9d7177250f84c8d914b97b8e193
altairo2087 <altairo2087@gmail.com>
2014-09-25 14:40:42 +0400

    Update model.php

--- model.php
+++ model.php
@@ -2,14 +2,21 @@
 
 class DataTable extends CActiveRecord 
 {
+  // здесь идентификатор таблицы данных
   protected tableId;
   
-  public function tableName() {
-    return "{{data_table}}".$this->tableId;
-  }
-  
+  // переопределяем конструктор, 
+  // добавляя установку идентификатора таблицы данных
   public function __construct($config=[], $tableId) {
     $this->tableId = $tableId;
     parent::__construct($config);
   }
+  
+  // переопределяем функцию, которая возвращает имя таблицы данных
+  public function tableName() {
+    return "{{data_table}}".$this->tableId;
+  }
 }
+
+$table = new DataTable(array(), 43);
+echo $table->tableName(); // cb_data43
