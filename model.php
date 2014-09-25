5c4e49c8d221651ffeec465a68e78927469350e7
altairo2087 <altairo2087@gmail.com>
2014-09-25 14:36:08 +0400

    Update model.php

--- model.php
+++ model.php
@@ -1,1 +1,15 @@
 <?php
+
+class DataTable extends CActiveRecord 
+{
+  protected tableId;
+  
+  public function tableName() {
+    return "{{data_table}}".$this->tableId;
+  }
+  
+  public function __construct($config=[], $tableId) {
+    $this->tableId = $tableId;
+    parent::__construct($config);
+  }
+}
